ec628a145697544451646cecbc1921ed77f7a5a8
Rahul Yadav <rjyadav909701@gmail.com>
2026-01-18 03:45:24 +0530

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,77 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".reset-btn");
+let newGameBtn = document.querySelector(".play-again");
+let winnerContainer = document.querySelector(".winner");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+     document.querySelector("main").style.display="";
+    winnerContainer.classList.add("hide");
+};
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+
+    }
+};
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, ${winner} is Winner`;
+    winnerContainer.classList.remove("hide");
+    disableBoxes();
+    disableGame();
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 && pos1 === pos2 && pos2 === pos3) {
+            showWinner(pos1);
+            return;
+        }
+    }
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        box.innerText = turnO ? "O" : "X";
+        turnO = !turnO;
+        box.disabled = true;
+        checkWinner();
+        
+    });
+});
+
+newGameBtn.addEventListener("click", resetGame);
+resetbtn.addEventListener("click", resetGame);
+
+let game=document.querySelector(".game-box");
+const disableGame=()=>{
+    document.querySelector("main").style.display="none";
+}
